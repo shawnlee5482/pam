7931f9685d6ff047495c6d963c2e36a9f4a36735
hapuum <lbklbk9345@gmail.com>
2023-11-04 09:53:47 -0700

    init

---
+++ php/src/index.php
@@ -0,0 +1,12 @@
+<?	
+    $host = 'db'; // service name from docker-compose.yml
+	$user = 'devuser';
+	$password = 'devpass';
+	$db = 'test_db';
+
+	$conn = new mysqli($host, $user, $password, $db);
+	if($conn -> connect_error){
+		echo 'connection failed' . $conn -> connect_error;
+	}
+	echo 'Sucessfully connected to MYSQL';
+?>
